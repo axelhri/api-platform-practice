c8b99e61c75019d30501c28d5133b4df5bc53479
Axelhri <axelhouairi@gmail.com>
2025-08-01 10:16:42 +0200

    feat(auth): jwt creation listener

--- src/DataPersister/BlogPostDataPersister.php
+++ src/DataPersister/BlogPostDataPersister.php
@@ -5,18 +5,19 @@ namespace App\DataPersister;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
 use App\Entity\BlogPost;
+use App\Repository\FolkRepository;
 use Symfony\Bundle\SecurityBundle\Security;
 
 class BlogPostDataPersister implements ProcessorInterface {
-    public function __construct(private Security $security, private ProcessorInterface $processor)
+    public function __construct(private Security $security, private ProcessorInterface $processor, private FolkRepository $folkRepository)
     {
     }
 
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): mixed
     {
-                $user = $this->security->getUser();
-                $data->setAuthor($user);
-                return $this->processor->process($data, $operation, $uriVariables, $context);
+        $user = $this->security->getUser();
+        $data->setAuthor($user);
+        return $this->processor->process($data, $operation, $uriVariables, $context);
     }
 
 }
---
+++ src/Middlewares/JWTCreatedListener.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Middlewares;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+
+class JWTCreatedListener {
+    public function onJWTCreated(JWTCreatedEvent $event)
+    {
+        $user = $event->getUser();
+
+        $payload = $event->getData();
+
+        $payload['username'] = $user->getEmail();
+
+        $event->setData($payload);
+    }
+}
