7c49b3a317318c1e19dbe8a98fa976f29b71ad1f
Axelhri <axelhouairi@gmail.com>
2025-08-22 17:22:52 +0200

    [FIX]: sorted the migrations to work properly

--- migrations/Version20250822152155.php
+++ migrations/Version20250822152155.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250822151441 extends AbstractMigration
+final class Version20250822152155 extends AbstractMigration
 {
     public function getDescription(): string
     {
